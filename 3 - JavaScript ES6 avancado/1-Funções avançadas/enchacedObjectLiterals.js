fc939e433c5bf98fde3b1ed51bc44054a0cbe4ae
wallanmota <wallanmota.13@gmail.com>
2021-12-30 15:54:59 -0300

    object literal

---
+++ 3 - JavaScript ES6 avancado/1-Funções avançadas/enchacedObjectLiterals.js
@@ -0,0 +1,13 @@
+// Objeto literal classico
+var obj = {
+    prop1 : 'Valor da propriedade'
+};
+console.log('OBJ = ', obj)
+
+// Referenciando variavel
+var prop = 'Valor da propriedade';
+var obj2 = {
+    // prop: prop == forma tradicional
+    prop // posso omitir
+};
+console.log('OBJ2 = ', obj2)
