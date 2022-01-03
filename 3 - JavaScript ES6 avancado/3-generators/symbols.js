74d0f2947ce106a940f008b7ab154f1a9aefde9c
wallanmota <wallanmota.13@gmail.com>
2022-01-03 19:32:43 -0300

    symbols e iterators

---
+++ 3 - JavaScript ES6 avancado/3-generators/symbols.js
@@ -0,0 +1,57 @@
+//Symbols - Identificador único
+
+const uniqueID = Symbol('Hello') //Valor serve apenas para debug
+const uniqueID2 = Symbol('Hello')
+
+console.log(uniqueID === uniqueID2)
+
+//Pode ser usado para gerar propriedades 'privadas' (ocultar a propriedade)
+
+const obj = {
+    [uniqueID]: 'Hello'
+}
+console.log(obj)
+
+//Well know symbols
+Symbol.iterator;
+Symbol.split;
+Symbol.toStringTag;
+
+const arr = [1, 2, 3, 4];
+const interador = arr[Symbol.iterator]();
+
+console.log(interador.next())
+console.log(interador.next())
+console.log(interador.next())
+console.log(interador.next())
+console.log(interador.next()) //vai retornar true pq a interação sobre a estrutura de dados acabou e traz um valor indefinido
+
+const str = 'Wallan Mota'
+for (let value of str){
+    console.log(value)
+}
+
+//SYMBOL ITERATOR
+    //Função para tornar o obj interavel
+    const obj2 = {
+        values: [1, 2, 3, 4],
+        [Symbol.iterator](){
+            let indice = 0;
+
+            return{
+                next: ()=>{
+                    indice++
+                    return{
+                        value: this.values[indice - 1],
+                        done: indice > this.values.length
+                    }
+                }
+            }
+        }
+    }
+    const it = obj2[Symbol.iterator]()
+
+    for (let value of obj2){
+        console.log(value)
+    }
+    
