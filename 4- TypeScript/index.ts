635b9f26d89df26b2e44c2ebd7d3f8fe5732d17d
wallanmota <wallanmota.13@gmail.com>
2022-01-05 16:05:52 -0300

    typescript

---
+++ 4- TypeScript/index.ts
@@ -0,0 +1,43 @@
+// Tipando parametros de funções
+function sum(a: number, b:number){
+    return a + b
+};
+
+// Interfaces
+interface iAnimal{
+    nome: string;
+    tipo: 'terrestre' | 'aquático'
+    domestico: boolean
+}
+
+interface iFelinos extends iAnimal{
+    visaoNoturna: boolean;
+}
+
+interface iCanino extends iAnimal{
+    porte: 'Grande' | 'Medio' | 'Pequeno'
+}
+
+const animal: iAnimal = {
+    nome: 'Elefante',
+    tipo: 'aquático',
+    domestico: false
+    
+} 
+const felino: iFelinos = {
+    nome:'Leão',
+    tipo: 'terrestre',
+    domestico: false,
+    visaoNoturna: true
+
+}
+
+// Types - pode receber mais de uma interface
+type iDomestico = iFelinos | iCanino
+
+const animal2: iDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'Medio',
+    tipo: 'terrestre',
+}
