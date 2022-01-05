c0cc2eddfa0e84c20a9b0d2b29c33f96e6df51c9
wallanmota <wallanmota.13@gmail.com>
2022-01-05 14:48:10 -0300

    sinon

--- 3 - JavaScript ES6 avancado/5-Qualidade código e Testes/MochaTool/src/math.js
+++ 3 - JavaScript ES6 avancado/5-Qualidade código e Testes/MochaTool/src/math.js
@@ -11,5 +11,8 @@ class Math{
     multiply(a, b){
         return a*b
     }
+    printSum(req, res, a, b){
+        res.load('index', a+b)
+    }
 }
 module.exports = Math
--- 3 - JavaScript ES6 avancado/5-Qualidade código e Testes/MochaTool/test/math.spec.js
+++ 3 - JavaScript ES6 avancado/5-Qualidade código e Testes/MochaTool/test/math.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
-const Math = require('../src/math.js')
-const expect = require('chai').expect //CHAI == ferramenta de assert
+const Math = require('../src/math.js');
+const expect = require('chai').expect; //CHAI == ferramenta de assert
+const sinon = require('sinon'); 
 
 describe('Math class', function(){
     it('Sum two numbers', function(){
@@ -33,6 +34,18 @@ describe('Math class', function(){
         expect(obj).to.have.property('name').equal('Wallan Mota')
         // expect(obj).to.equal(obj2) //Assim vai dar erro pois ele nao verifica as propriedades do obj
         expect(obj).to.deep.equal(obj2)
+    })
+    it('Calls req with sum and index values', function(){
+         const req = {};
+         const res = {
+             load: sinon.spy() //consigo verificar se ela foi invocada da maneira correta.
+         }
+         const math = new Math();
+
+         math.printSum(req, res, 5, 5);
+         expect(res.load.calledOnce).to.be.true;
+         expect(res.load.args[0][0]).to.equal('index')
+         expect(res.load.args[0][1]).to.equal(10)
     })
 
 })
