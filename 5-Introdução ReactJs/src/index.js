c08d0ba137e292279e2966d7683713260872240e
wallanmota <wallanmota.13@gmail.com>
2022-01-06 18:00:41 -0300

    renderizando elementos / criando component

--- 5-Introdução ReactJs/src/index.js
+++ 5-Introdução ReactJs/src/index.js
@@ -1,26 +1,32 @@
 import React from "react"
 import ReactDOM  from "react-dom"
+import Button from './Button'
 import "./styles.css"
 
-let sum = (a,b) => a+b
+let sum = (a,b) => alert(a+b)
 
 function primeiroJSX(){ //JSX posso criar html dentro de funções
     return(
         <div>
             <h1 className="test">Hello World!!!</h1> 
-            <h2>Soma: {sum(10, 10)} </h2>
         </div>
     )
 }
 
-const App = () => {
+const element = 'Digital Innovation One'
+const element2 = <h1>Olá turma =D</h1>
+
+const App = () => { //Component
     
     return(
         <div className="App">
             {primeiroJSX()} {/* Função JS no react deve estar dentro de {} */}
+            {element}
+            {element2}
+            <Button onClick ={()=>sum(10, 5)} name='Resultado da soma'/>
         </div>
     )
 }
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement) //Renderiza
