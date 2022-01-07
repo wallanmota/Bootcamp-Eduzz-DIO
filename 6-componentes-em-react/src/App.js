15ce4cc5d5a5a6c45ef3e4c49ba6eacde6e86605
wallanmota <wallanmota.13@gmail.com>
2022-01-07 14:59:15 -0300

    preparando ambiente e hello world

---
+++ 6-componentes-em-react/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return(
+    <h1>Hello World!</h1>
+  )
+}
+
+export default App
+
+// export default function App(){
+//   return(
+//     <h1>Hello World!</h1>
+//   )
+// }
---
+++ 6-componentes-em-react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(//O redender recebe 2 parametros
+    <App />,  //O que vai ser renderizado 
+  document.getElementById('root') //Onde vai ser renderizado
+);
+
+
