15cad174125763378ef3c207279868fc1215202f
wallanmota <wallanmota.13@gmail.com>
2022-01-07 16:21:25 -0300

    estrutura de um componente

--- 6-componentes-em-react/src/App.js
+++ 6-componentes-em-react/src/App.js
@@ -1,6 +1,23 @@
 const App = () => {
-  return(
-    <h1>Hello World!</h1>
+  return( //react fragment (<> </>) trabalhar com componentes irmão se nao quiser usar div
+    <> 
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 1
+        </a>
+      
+      
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 2
+        </a>
+      
+      
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
+          Item 3
+        </a>
+      </ul>
+    </>
   )
 }
 
--- 6-componentes-em-react/src/index.js
+++ 6-componentes-em-react/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 
 
 ReactDOM.render(//O redender recebe 2 parametros
-    <App />,  //O que vai ser renderizado 
-  document.getElementById('root') //Onde vai ser renderizado
+    <App />,  //1- O que vai ser renderizado 
+  document.getElementById('root') //2- Onde vai ser renderizado
 );
 
 
